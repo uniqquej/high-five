3263e641a560d4b094c9e5f09ba0ebe59b3bcabf
uniqquej <cjy4477@gmail.com>
2023-08-23 17:15:41 +0900

    face detect video

--- routes/face.js
+++ routes/face.js
@@ -4,5 +4,8 @@ const router = express.Router();
 router.get('/', function(req,res){
     res.render('face')
 })
+router.get('/video', function(req,res){
+    res.render('video')
+})
 
 module.exports = router;

9497b2b4ba415c62dcf0c80474dc9074b97b61b3
uniqquej <cjy4477@gmail.com>
2023-08-28 03:25:11 +0900

    카메라 감지 수정

--- routes/face.js
+++ routes/face.js
@@ -1,9 +1,37 @@
 const express = require('express');
+const PatternModel   = require('../models/sleep_pattern')
+
 const router = express.Router();
 const auth        = require('./auth');
 
 router.get('/',auth.CheckAuth, function(req,res){
     res.render('video', {user: req.user})
+})
+
+router.post('/', async function(req,res){
+    const user_id = req.body.user_id
+    const count = req.body.count;
+    
+    console.log(user_id, count);
+
+        var pattern = {
+            user_id: user_id,
+            count: count,
+        };
+        const result = await PatternModel.AddPattern(pattern);
+        if (result.error) {
+            console.log(result.error);
+            res.status(400).send({
+                error : 'something wrong'
+            })
+        }
+        else {
+            console.log('ADD PATTERN');
+        }
+        res.status(200).send({
+            msg : 'ADD PATTERN'
+        })
+
 })
 
 module.exports = router;
